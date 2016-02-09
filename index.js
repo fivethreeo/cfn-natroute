26421e778e6d88891152ad341195090a80006f22
Øyvind Saltvik <oyvind.saltvik@gmail.com>
2016-02-09 22:47:00 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,47 @@
+
+var AWS = require('aws-sdk');
+var CfnLambda = require('cfn-lambda');
+
+var EC2 = new AWS.EC2({apiVersion: '2015-01-01'});
+var Lambda = new AWS.Lambda({apiVersion: '2015-03-31'});
+
+var Delete = CfnLambda.SDKAlias({
+  api: EC2,
+  method: 'deleteRoute',
+  ignoreErrorCodes: [404, 409],
+  keys: [
+    "DestinationCidrBlock",
+    "RouteTableId"
+  ]
+});
+
+var BoolProperties = [
+];
+
+var NumProperties = [
+];
+
+var Create = CfnLambda.SDKAlias({
+  api: EC2,
+  method: 'createRoute',
+  forceBools: BoolProperties,
+  forceNums: NumProperties,
+  keys: [
+    "DestinationCidrBlock",
+    "RouteTableId",
+    "NatGatewayId"
+  ]
+});
+
+exports.handler = CfnLambda({
+  Create: Create,
+  Update: Create,
+  Delete: Delete,
+  NoUpdate: NoUpdate,
+  TriggersReplacement: [
+    "DestinationCidrBlock",
+    "RouteTableId",
+    "NatGatewayId"
+  ],
+  SchemaPath: [__dirname, 'schema.json']
+});
